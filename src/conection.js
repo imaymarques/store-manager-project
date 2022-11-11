86cbf3ef0128c856f37e7a4d7d5e8b8a3cc04545
Mayara Marques <mayaramarquescosta@outlook.com>
2022-11-11 19:12:20 -0300

    Requisito 1

--- src/conection.js
+++ src/conection.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const connection = mysql.createPool({
-  host: 'db',
-  user: 'root',
-  password: 'password',
-  database: 'StoreManager',
+  host: process.env.MYSQL_HOST || 'db',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'password',
+  database: process.env.MYSQL_DATABASE || 'StoreManager',
 });
 
 module.exports = connection;
